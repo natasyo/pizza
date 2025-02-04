512afacfa1b4ddb2bb0846c3f49b45aed88e77d5
natasyo <natasyo23@gmail.com>
2025-02-04 13:26:45 +0300

    init: project

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { resolve } from 'path'
+
+export default {
+    root: resolve(__dirname, 'src'),
+    build: {
+        outDir: '../dist'
+    },
+    server: {
+        port: 8080
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true,
+            },
+        },
+    },
+}
