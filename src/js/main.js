acf16b7390366b537b0a1729b05aac328ecd5fa2
natasyo <natasyo23@gmail.com>
2025-02-06 11:58:20 +0300

    feat: mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,16 @@ import "../scss/styles.scss";
 
 // // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+
+//
+document.addEventListener("click", function (event) {
+  const navbarCollapse = document.querySelector(".navbar-collapse.show"); // Открытое меню
+  if (
+    navbarCollapse &&
+    // !navbarCollapse.contains(event.target) &&
+    !event.target.closest(".navbar-toggler") &&
+    !event.target.closest(".navbar-nav")
+  ) {
+    new bootstrap.Collapse(navbarCollapse).hide();
+  }
+});
